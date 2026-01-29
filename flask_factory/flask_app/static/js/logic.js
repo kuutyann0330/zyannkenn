aad6904cd00d656982893c429b7411ca956ec320
kuutyann0330 <kuutyann0330@gmail.com>
2026-01-30 00:16:41 +0900

    Add files via upload

--- flask_factory/flask_app/static/js/logic.js
+++ flask_factory/flask_app/static/js/logic.js
@@ -63,24 +63,36 @@ const createHandLandmarker = async () => {
     navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
       //videoタグに対して取得した映像を張り付けしている
       video.srcObject = stream;
-      //videoタグがデータがロードし始めたらイベントを開始する
+      //videoタグがデータがロードし始めたらイベント(カメラの描画サイズを計算)を開始する
       video.addEventListener("loadeddata", predictWebcam);
     });
   }
 
-    画面サイズが変更されても表示がバグるのを防ぐために
+    //画面サイズが変更されても表示がバグるのを防ぐために
     let lastVideoTime = -1;
     let results = undefined;
     console.log(video);
     async function predictWebcam() {
       // video.videoWidth = 640　ブラウザ側が動画データに合わせてサイズを入れてくれる
+      // style.width：キャンパスのブラウザ上の大きさ
       canvasElement.style.width = video.videoWidth;
       canvasElement.style.height = video.videoHeight;
+      // width：キャンパスの解像度を入れる
       canvasElement.width = video.videoWidth;
       canvasElement.height = video.videoHeight;
+      // video.videoHeightを両方入れ揃えないとウィンドウのサイズなどでランドマークの位置がずれて表示される！！
     
-    
-    
-    
+      // ▽▽ここは念のために書いている場所です本来は必要ありません！▽▽
+      // AIのモードが静止画になっているなら動画ように切り替えます！
+      if(runningMode == "IMAGE"){
+        runningMode = "VIDEO"; //ここで現在のモードをビデオだよってメモする
+        await handLandmarker.setOptions({ runningMode: "VIDEO" }); //実際にAIにあなたはビデオを監視してねって伝える！
+        // 同じようなことを2回している理由は下のコードがawait(非同期)で実行することになっているので処理が遅くなっているから
+        // 値が完全にビデオモードになる前に他の関数から現在の状態を聞かれた時の為にすぐ切り替えられる runningMode = "VIDEO"; で他の関数に伝える
+      }
+      // △△ここは念のために書いている場所です本来は必要ありません！△△
+      
+      
+
     }
   
